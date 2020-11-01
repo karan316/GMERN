b746ad5c0b64e980894ad25ff657103120178914
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-11-01 12:16:02 +0530

    added likeCount, commentCount modifiers

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,6 +2,16 @@ const postsResolvers = require("./posts");
 const usersResolvers = require("./users");
 const commentsResolvers = require("./comments");
 module.exports = {
+    // Post modifier -> each type any Post query, mutation or subscription occurs it goes through this modifier
+    Post: {
+        likeCount(parent) {
+            // parent will have the post/s for which the query, mutation or subscription was called
+            return parent.likes.length;
+        },
+        commentCount(parent) {
+            return parent.comments.length;
+        },
+    },
     Query: {
         ...postsResolvers.Query,
     },
